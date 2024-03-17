e03bb055cb3017da7746ee6fb28a328e6a751149
Josuah Demangeon <me@josuah.net>
2024-03-17 05:57:26 +0100

    move si5351 PLL driver in-tree, out of the example

--- src/i2cmux.c
+++
@@ -1,8 +0,0 @@
-// An I2C MUX exists at either 0x70 or 0x71 depending on the slot used
-#define I2C_MUX_ADDR 0x70
-
-static void sel_cam(uint8_t slot, uint8_t cam_id) {
-	printf("Selecting camera %d\n\r", cam_id);
-	uint8_t val = 0x4 + cam_id;
-	i2c_write(I2C_MUX_ADDR + slot, 0, &val, 1, 1);
-}
--- src/main.c
+++ src/main.c
@@ -1,14 +1,11 @@
 #include <zephyr/kernel.h>
-#include <zephyr/shell/shell.h>
+#include <zephyr/devicetree.h>
 #include <zephyr/drivers/usb/udc.h>
-#include <zephyr/drivers/i2c.h>
-#include <zephyr/drivers/uart.h>
+#include <zephyr/drivers/clock_control.h>
 #include <zephyr/usb/usbd.h>
 #include <zephyr/usb/bos.h>
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
-
-#include "si5351.h"
 
 USBD_DEVICE_DEFINE(my_usbd_dev, DEVICE_DT_GET(DT_NODELABEL(zephyr_udc0)), 0x1209, 0x0001);
 USBD_DEVICE_QUALIFIER_DEFINE(my_usbd_device_qualifier);
@@ -51,22 +48,11 @@ static struct usbd_desc_node my_usbd_bos = {
 	.desc = &my_usbd_bos_desc,
 };
 
-void si5351_i2c_write(uint8_t i2c_addr, uint8_t reg_addr, uint8_t reg_data)
-{
-	i2c_reg_write_byte(DEVICE_DT_GET(DT_NODELABEL(i2c0)),
-			i2c_addr, reg_addr, reg_data);
-}
-
 int main(void)
 {
 	int err = 0;
 
-	/* Configure the external PLL over I2C, switching the CPU clock over it.
-	 * This is done by hardware, no software config to make that happen. */
-	k_sleep(K_MSEC(100));
-	LOG_INF("Initializing the si5351 PLL");
-	si5351_init();
-	k_sleep(K_MSEC(100));
+	clock_control_on(DEVICE_DT_GET(DT_NODELABEL(pll0)), NULL);
 
 	err = usbd_add_descriptor(&my_usbd_dev, &my_usbd_device_qualifier);
 	err |= usbd_add_descriptor(&my_usbd_dev, &my_usbd_bos);
