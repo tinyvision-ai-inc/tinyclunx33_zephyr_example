5920d8a10a4c6a60a11c0e928bba26935341f3b2
Josuah Demangeon <me@josuah.net>
2024-03-27 00:45:42 +0100

    high-thrpughput demo

--- usb_throughput_example/src/main.c
+++ usb_throughput_example/src/main.c
@@ -1,9 +1,11 @@
 #include <zephyr/kernel.h>
 #include <zephyr/devicetree.h>
-#include <zephyr/drivers/usb/udc.h>
 #include <zephyr/drivers/clock_control.h>
+#include <zephyr/drivers/usb/udc.h>
 #include <zephyr/usb/usbd.h>
 #include <zephyr/usb/bos.h>
+#include <zephyr/usb/class/usb_cdc_raw.h>
+
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
@@ -48,11 +50,19 @@ static struct usbd_desc_node my_usbd_bos = {
 	.desc = &my_usbd_bos_desc,
 };
 
+/* Static pool of buffer with no data in it */
+NET_BUF_POOL_DEFINE(_buf_pool, 2, 0, sizeof(struct udc_buf_info), NULL);
+
 int main(void)
 {
-	int err = 0;
+	const struct device *udc0 = DEVICE_DT_GET(DT_NODELABEL(zephyr_udc0));
+	const struct device *raw0 = DEVICE_DT_GET(DT_NODELABEL(raw0));
+	struct net_buf *buf;
+	int err;
 
+	k_sleep(K_MSEC(200));
 	clock_control_on(DEVICE_DT_GET(DT_NODELABEL(pll0)), NULL);
+	k_sleep(K_MSEC(200));
 
 	err = usbd_add_descriptor(&my_usbd_dev, &my_usbd_device_qualifier);
 	err |= usbd_add_descriptor(&my_usbd_dev, &my_usbd_bos);
@@ -61,7 +71,7 @@ int main(void)
 	err |= usbd_add_descriptor(&my_usbd_dev, &my_usbd_product);
 	//err |= usbd_add_descriptor(&my_usbd_dev, &my_usbd_serial_number);
 	err |= usbd_add_configuration(&my_usbd_dev, &my_usbd_config);
-	err |= usbd_register_class(&my_usbd_dev, "uvc_0", 1);
+	err |= usbd_register_class(&my_usbd_dev, "cdc_raw_0", 1);
 	__ASSERT_NO_MSG(err == 0);
 
 	err = usbd_init(&my_usbd_dev);
@@ -72,9 +82,22 @@ int main(void)
 
 	/* As soon as interrupts are enabled in the hardware architecture, this
 	 * can be removed */
+	for (int i = 0; i < 100; i++) {
+		usb23_irq_handler(udc0);
+		k_sleep(K_MSEC(1));
+	}
+
+	LOG_INF("Starting USB3 transfer");
+
+	/* Allocate a buffer with existing data */
+	buf = net_buf_alloc_with_data(&_buf_pool,
+		(void *)0xb1100000, 1024 * 63, Z_TIMEOUT_NO_WAIT);
+	__ASSERT_NO_MSG(buf != NULL);
+
 	while (true) {
-		k_sleep(K_USEC(10));
-		usb23_irq_handler(DEVICE_DT_GET(DT_NODELABEL(zephyr_udc0)));
+		usb23_irq_handler(udc0);
+		k_sleep(K_MSEC(1));
+		cdc_raw_write(raw0, buf);
 	}
 
 	return 0;
