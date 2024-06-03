b29d745c56b458e34af4b426ad93542f702e8893
Josuah Demangeon <me@josuah.net>
2024-06-03 19:51:08 +0200

    throughput: turn it into a test for USB Manager

--- example_usb_throughput/src/main.c
+++ example_usb_throughput/src/main.c
@@ -7,9 +7,15 @@
 #include <zephyr/usb/class/usb_cdc_raw.h>
 
 #include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
+LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
-USBD_DEVICE_DEFINE(my_usbd, DEVICE_DT_GET(DT_NODELABEL(zephyr_udc0)), 0x1209, 0x0001);
+/* Grab the devices from the devicetree */
+#define UDC0 DEVICE_DT_GET(DT_NODELABEL(zephyr_udc0))
+#define CDC0 DEVICE_DT_GET(DT_NODELABEL(cdc0))
+#define PLL0 DEVICE_DT_GET(DT_NODELABEL(pll0))
+
+/* USB descriptors and other definitions */
+USBD_DEVICE_DEFINE(my_usbd, UDC0, 0x1209, 0x0001);
 USBD_DEVICE_QUALIFIER_DEFINE(my_usbd_dev_qualifier);
 USBD_CONFIGURATION_DEFINE(my_usbd_config, USB_SCD_SELF_POWERED, 100);
 USBD_BOS_DEFINE(my_usbd_bos);
@@ -18,38 +24,33 @@ USBD_DESC_MANUFACTURER_DEFINE(my_usbd_manufacturer, "tinyVision.ai");
 USBD_DESC_PRODUCT_DEFINE(my_usbd_product, "tinyCLUNX33");
 //USBD_DESC_SERIAL_NUMBER_DEFINE(my_usbd_serial_number, "0123456789ABCDEF");
 
-/* Static pool of empty buffers: pointer given externally */
-NET_BUF_POOL_DEFINE(_pool, 1, 0, sizeof(struct udc_buf_info), NULL);
+/* Static pool of buffer with no data in it */
+NET_BUF_POOL_DEFINE(_buf_pool, 2, 0, sizeof(struct udc_buf_info), NULL);
 
 #define RAM1_ADDR DT_REG_ADDR(DT_NODELABEL(ram1))
 #define RAM1_SIZE DT_REG_SIZE(DT_NODELABEL(ram1))
 
 static int _write_callback(const struct device *dev, struct net_buf *buf, int err)
 {
-	struct udc_buf_info *bi = udc_get_buf_info(buf);
+	cdc_raw_write(dev, buf, false);
+}
 
-	if (err) {
-		LOG_ERR("USB write error %d", err);
-		return err;
+void sleep_ms(uint32_t msec)
+{
+	for (int ms = 0; ms < msec / 2; ms += 10) {
+		k_sleep(K_MSEC(10));
+		usb23_irq_handler(UDC0);
 	}
-	LOG_DBG("%s: buf=%p data=%p len=%d size=%d zlp=%d", __func__,
-		buf, buf->data, buf->len, buf->size, bi->zlp);
-
-	/* Freeing here will unblock the memory pool */
-	net_buf_unref(buf);
-	return 0;
 }
 
 int main(void)
 {
-	const struct device *cdc0 = DEVICE_DT_GET(DT_NODELABEL(cdc0));
-	const struct device *pll0 = DEVICE_DT_GET(DT_NODELABEL(pll0));
 	struct net_buf *buf;
 	int err;
 
-	k_sleep(K_MSEC(500));
-	clock_control_on(pll0, NULL);
-	k_sleep(K_MSEC(500));
+	k_sleep(K_MSEC(200));
+	clock_control_on(PLL0, NULL);
+	k_sleep(K_MSEC(200));
 
 	err = usbd_add_descriptor(&my_usbd, &my_usbd_dev_qualifier);
 	err |= usbd_add_descriptor(&my_usbd, &my_usbd_bos);
@@ -67,23 +68,28 @@ int main(void)
 	err = usbd_enable(&my_usbd);
 	__ASSERT_NO_MSG(err == 0);
 
-	/* Glue with the CDC RAW so that we can react to read and write completion. */
+	/* Hook the callback called used once a write is completed */
 	cdc_raw_set_write_callback(cdc0, &_write_callback);
 
-	LOG_DBG("Waiting a serial console 0 to connect...");
-	while (!cdc_raw_is_ready(cdc0)) {
-		k_sleep(K_MSEC(100));
+	/* Allocate a buffer with existing data */
+	buf = net_buf_alloc_with_data(&_buf_pool, (void *)0xb1200000, 128 * 1024, K_NO_WAIT);
+	__ASSERT_NO_MSG(buf != NULL);
+
+	LOG_DBG("0");
+
+	/* Wait that a serial console connects */
+	while (!cdc_raw_is_ready(CDC0)) {
+		sleep_ms(100);
 	}
 
-	LOG_DBG("Waiting a bit before starting the stream...");
-	k_sleep(K_SECONDS(1));
+	for (int i = 3; i > 0; i--) {
+		LOG_DBG("sending something in %d", i);
+		sleep_ms(600);
+	}
+	cdc_raw_write(CDC0, buf, false);
 
-	LOG_DBG("Starting the bulk throughput test...");
 	while (true) {
-		/* This will wait until _write_callback frees some more buffers */
-		buf = net_buf_alloc_with_data(&_pool, (void *)RAM1_ADDR, RAM1_SIZE, K_FOREVER);
-		__ASSERT_NO_MSG(buf != NULL);
-		cdc_raw_write(cdc0, buf, false);
+		sleep_ms(10000);
 	}
 
 	return 0;
