ab3fcb41c151f42997f28986af417dc92abb43f7
Josuah Demangeon <me@josuah.net>
2024-06-28 00:10:07 +0200

    cdc_throughput: make it compatible with latest Zephyr

--- example_usb_cdc_throughput/src/main.c
+++ example_usb_cdc_throughput/src/main.c
@@ -13,18 +13,37 @@ LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 #define UDC0 DEVICE_DT_GET(DT_NODELABEL(zephyr_udc0))
 #define CDC0 DEVICE_DT_GET(DT_NODELABEL(cdc0))
 #define PLL0 DEVICE_DT_GET(DT_NODELABEL(pll0))
-#define RAM1_ADDR DT_REG_ADDR(DT_NODELABEL(ram1))
-#define RAM1_SIZE DT_REG_SIZE(DT_NODELABEL(ram1))
+#define DUMP_ADDR 0xb1100000
+#define DUMP_SIZE (15*1024*1024)
 
-/* USB descriptors and other definitions */
+#define SPEEDS (USB_BOS_SPEED_SUPERSPEED_GEN1 | USB_BOS_SPEED_HIGHSPEED | USB_BOS_SPEED_FULLSPEED)
+
+static const struct usb_bos_capability_superspeed_usb bos_cap_ss = {
+	.bLength = sizeof(struct usb_bos_capability_superspeed_usb),
+	.bDescriptorType = USB_DESC_DEVICE_CAPABILITY,
+	.bDevCapabilityType = USB_BOS_CAPABILITY_SUPERSPEED_USB,
+	.bmAttributes = 0,
+	.wSpeedsSupported = sys_cpu_to_le16(SPEEDS),
+	.bFunctionnalSupport = 1,
+	.bU1DevExitLat = 10,
+	.wU2DevExitLat = sys_cpu_to_le16(1023),
+};
+USBD_DESC_BOS_DEFINE(my_usbd_bos_cap_ss, sizeof(bos_cap_ss), &bos_cap_ss);
+
+static const struct usb_bos_capability_lpm bos_cap_lpm = {
+	.bLength = sizeof(struct usb_bos_capability_lpm),
+	.bDescriptorType = USB_DESC_DEVICE_CAPABILITY,
+	.bDevCapabilityType = USB_BOS_CAPABILITY_EXTENSION,
+	.bmAttributes = USB_BOS_ATTRIBUTES_LPM,
+};
+USBD_DESC_BOS_DEFINE(my_usbd_bos_cap_lpm, sizeof(bos_cap_lpm), &bos_cap_lpm);
+
 USBD_DEVICE_DEFINE(my_usbd, UDC0, 0x1209, 0x0001);
-USBD_DEVICE_QUALIFIER_DEFINE(my_usbd_dev_qualifier);
 USBD_CONFIGURATION_DEFINE(my_usbd_config, USB_SCD_SELF_POWERED, 100);
-USBD_BOS_DEFINE(my_usbd_bos);
-USBD_DESC_LANG_DEFINE(my_usbd_lang);
 USBD_DESC_MANUFACTURER_DEFINE(my_usbd_manufacturer, "tinyVision.ai");
 USBD_DESC_PRODUCT_DEFINE(my_usbd_product, "tinyCLUNX33");
-//USBD_DESC_SERIAL_NUMBER_DEFINE(my_usbd_serial_number, "0123456789ABCDEF");
+USBD_DESC_LANG_DEFINE(my_usbd_lang);
+USBD_DESC_SERIAL_NUMBER_DEFINE(my_usbd_serial_number);
 
 /* Static pool of buffer with no data in it */
 NET_BUF_POOL_DEFINE(_buf_pool, 100, 0, sizeof(struct udc_buf_info), NULL);
@@ -49,33 +68,33 @@ void sleep_ms(uint32_t msec)
 {
 	for (int ms = 0; ms < msec; ms += 20) {
 		k_sleep(K_MSEC(10));
-		usb23_irq_handler(UDC0);
+		//usb23_irq_handler(UDC0);
 	}
 }
 
 int main(void)
 {
 	struct net_buf *buf;
-	int err;
+	int err = 0;
 
-	k_sleep(K_MSEC(200));
-	clock_control_on(PLL0, NULL);
-	k_sleep(K_MSEC(200));
-
-	err = usbd_add_descriptor(&my_usbd, &my_usbd_dev_qualifier);
-	err |= usbd_add_descriptor(&my_usbd, &my_usbd_bos);
 	err |= usbd_add_descriptor(&my_usbd, &my_usbd_lang);
 	err |= usbd_add_descriptor(&my_usbd, &my_usbd_manufacturer);
 	err |= usbd_add_descriptor(&my_usbd, &my_usbd_product);
+	err |= usbd_add_descriptor(&my_usbd, &my_usbd_bos_cap_lpm);
+	err |= usbd_add_descriptor(&my_usbd, &my_usbd_bos_cap_ss);
 	//err |= usbd_add_descriptor(&my_usbd, &my_usbd_serial_number);
-	err |= usbd_add_configuration(&my_usbd, &my_usbd_config);
-	err |= usbd_register_class(&my_usbd, "cdc_raw_0", 1);
+	err |= usbd_add_configuration(&my_usbd, USBD_SPEED_SS, &my_usbd_config);
 	__ASSERT_NO_MSG(err == 0);
 
-	err = usbd_init(&my_usbd);
+	usbd_device_set_code_triple(&my_usbd, USBD_SPEED_SS, USB_BCC_MISCELLANEOUS, 0x02, 0x01);
+
+	err |= usbd_register_all_classes(&my_usbd, USBD_SPEED_SS, 1);
 	__ASSERT_NO_MSG(err == 0);
 
-	err = usbd_enable(&my_usbd);
+	err |= usbd_init(&my_usbd);
+	__ASSERT_NO_MSG(err == 0);
+
+	err |= usbd_enable(&my_usbd);
 	__ASSERT_NO_MSG(err == 0);
 
 	/* Hook the callback called used once a write is completed */
@@ -94,8 +113,9 @@ int main(void)
 	LOG_INF("Starting the bulk throughput test...");
 	while (true) {
 		/* This will wait until _write_callback frees some more buffers */
-		buf = net_buf_alloc_with_data(&_buf_pool, (void *)RAM1_ADDR, RAM1_ADDR, K_NO_WAIT);
+		buf = net_buf_alloc_with_data(&_buf_pool, (void *)DUMP_ADDR, DUMP_SIZE, K_NO_WAIT);
 		if (buf != NULL) {
+			LOG_DBG("buf=%p size=%u len=%u data=%p", buf, buf->size, buf->len, buf->data);
 			cdc_raw_write(CDC0, buf, false);
 		}
 
