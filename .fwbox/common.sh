62430a6775ced62713fe7628a3e725756af3f476
Josuah Demangeon <me@josuah.net>
2024-07-03 00:35:51 +0200

    fwbox: usability fixes

--- .fwbox/common.sh
+++ .fwbox/common.sh
@@ -7,14 +7,18 @@ fwbox_do_flash_zephyr() {
     fwbox_flash_ecpprog 0x100000 <build/zephyr/zephyr.bin
 }
 
-fwbox_do_all() {
+fwbox_do_all() (
+    set -eu
+    west build
     fwbox_do_flash_zephyr
     fwbox_do_power_cycle
     fwbox_do_reset
-}
+)
 
-fwbox_do_video_capture() {
+fwbox_do_video_capture() (
+    set -eu 
+    echo "fwbox: capturing to localhost:/tmp/video0.mp4" >&2
     echo 0xffffffff | fwbox_run dd of=/sys/module/uvcvideo/parameters/trace
-    fwbox_run ffmpeg -y -t 3 -i /dev/video0 -f mp4 video0.mp4
-    fwbox_run cat video0.mp4 >video0.mp4
-}
+    fwbox_run ffmpeg -y -t 3 -i /dev/video0 /tmp/video0.mp4
+    fwbox_run cat /tmp/video0.mp4 >/tmp/video0.mp4
+)
