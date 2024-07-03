dc9f03646de6fdc463735b430e4109eae04c1644
Josuah Demangeon <me@josuah.net>
2024-07-03 15:59:43 +0200

    fwbox: detect if on local network or remote

--- .fwbox/sita.sh
+++ .fwbox/sita.sh
@@ -1,4 +1,12 @@
-FWBOX="ssh,host=172.22.0.2,port=22"
+if nc -vz 172.22.0.2 22; then
+    FWBOX="ssh,host=172.22.0.2,port=22"
+elif nc -vz 192.168.15.100 22; then
+    FWBOX="ssh,host=192.168.15.100,port=22"
+else
+    echo "could not reach any host to connect to" >&2
+    exit 1
+fi
+
 FWBOX_GPIO_RESET="x 22"
 FWBOX_GPIO_POWER="x 20"
 
--- .fwbox/sita_local.sh
+++
@@ -1,9 +0,0 @@
-FWBOX="ssh,host=192.168.15.100,port=22"
-FWBOX_GPIO_RESET="x 22"
-FWBOX_GPIO_POWER="x 20"
-
-fwbox_gpioset() {
-    fwbox_gpioset_micropython "$@"
-}
-
-. $FWBOX_PATH/common.sh
