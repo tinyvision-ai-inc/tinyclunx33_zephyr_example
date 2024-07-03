ec36bddf97e591522ae4b4edacea501e1036a363
Josuah Demangeon <me@josuah.net>
2024-07-03 03:39:51 +0200

    cdc_shell: add a test

---
+++ example_usb_cdc_shell/test.sh
@@ -0,0 +1,17 @@
+# sudo git clone https://github.com/firmware-box/fwbox /opt/fwbox
+. /opt/fwbox/fwbox.sh
+
+# Exit on first failure
+set -eu
+
+# Select which board to use for testing
+fwbox_use idefix
+
+# Build and upload the firmware
+fwbox_do_all
+
+# Give it time to enumerate
+sleep 5
+
+# Probe the console
+FWBOX="$FWBOX picocom,port=/dev/ttyACM1 repl" fwbox_run "device list" | grep usb@b0000000
