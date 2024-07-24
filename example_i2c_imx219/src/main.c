6f07e13b99c5b7f5990a48b72cdf8ed1636c8969
alanshaju <alanshaju@rideltech.com>
2024-07-24 16:28:31 +0530

    example code for testing IMX219 device driver

---
+++ example_i2c_imx219/src/main.c
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/video.h>
+#include <zephyr/drivers/i2c.h>
+
+void main(void)
+{
+    printf("hello\n");
+    k_sleep(K_SECONDS(5));
+    printf("test1......\n");
+
+    const struct device *video_dev = DEVICE_DT_GET(DT_NODELABEL(imx219));
+    if (video_dev == NULL || !device_is_ready(video_dev))
+    {
+        printf("Could not get I2C cam device\n");
+        return;
+    }
+	printk("Video device: %s\n", video_dev->name);
+
+}
