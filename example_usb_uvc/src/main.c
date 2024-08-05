4702ebbfa82a69af24fa1fb237e922532d0bc69a
Josuah Demangeon <me@josuah.net>
2024-08-05 02:35:36 +0200

    uvc: update to the new UVC API

--- example_usb_uvc/src/main.c
+++ example_usb_uvc/src/main.c
@@ -1,13 +1,14 @@
 #include <zephyr/kernel.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/usb/udc.h>
-#include <zephyr/drivers/clock_control.h>
+#include <zephyr/drivers/video.h>
 #include <zephyr/usb/usbd.h>
 #include <zephyr/usb/bos.h>
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(app, LOG_LEVEL_DBG);
 
 #define UDC0 DEVICE_DT_GET(DT_NODELABEL(zephyr_udc0))
+#define UVC0 DEVICE_DT_GET(DT_NODELABEL(uvc0))
 
 #define SPEEDS (USB_BOS_SPEED_SUPERSPEED_GEN1 | USB_BOS_SPEED_HIGHSPEED | USB_BOS_SPEED_FULLSPEED)
 
@@ -21,7 +22,7 @@ static const struct usb_bos_capability_superspeed_usb bos_cap_ss = {
 	.bU1DevExitLat = 10,
 	.wU2DevExitLat = sys_cpu_to_le16(1023),
 };
-USBD_DESC_BOS_DEFINE(my_usbd_bos_cap_ss, sizeof(bos_cap_ss), &bos_cap_ss);
+USBD_DESC_BOS_DEFINE(usbd_bos_cap_ss, sizeof(bos_cap_ss), &bos_cap_ss);
 
 static const struct usb_bos_capability_lpm bos_cap_lpm = {
 	.bLength = sizeof(struct usb_bos_capability_lpm),
@@ -29,38 +30,59 @@ static const struct usb_bos_capability_lpm bos_cap_lpm = {
 	.bDevCapabilityType = USB_BOS_CAPABILITY_EXTENSION,
 	.bmAttributes = USB_BOS_ATTRIBUTES_LPM,
 };
-USBD_DESC_BOS_DEFINE(my_usbd_bos_cap_lpm, sizeof(bos_cap_lpm), &bos_cap_lpm);
+USBD_DESC_BOS_DEFINE(usbd_bos_cap_lpm, sizeof(bos_cap_lpm), &bos_cap_lpm);
 
-USBD_DEVICE_DEFINE(my_usbd, UDC0, 0x1209, 0x0001);
-USBD_CONFIGURATION_DEFINE(my_usbd_config, USB_SCD_SELF_POWERED, 100);
-USBD_DESC_MANUFACTURER_DEFINE(my_usbd_manufacturer, "tinyVision.ai");
-USBD_DESC_PRODUCT_DEFINE(my_usbd_product, "tinyCLUNX33");
-USBD_DESC_LANG_DEFINE(my_usbd_lang);
-USBD_DESC_SERIAL_NUMBER_DEFINE(my_usbd_serial_number);
+USBD_DEVICE_DEFINE(usbd, UDC0, 0x1209, 0x0001);
+USBD_CONFIGURATION_DEFINE(usbd_config, USB_SCD_SELF_POWERED, 100);
+USBD_DESC_MANUFACTURER_DEFINE(usbd_manufacturer, "tinyVision.ai");
+USBD_DESC_PRODUCT_DEFINE(usbd_product, "tinyCLUNX33");
+USBD_DESC_LANG_DEFINE(usbd_lang);
 
 int main(void)
 {
-	int err = 0;
+	struct video_buffer vbuf = {0};
+	struct video_format fmt = {0};
+	int ret = 0;
 
-	err |= usbd_add_descriptor(&my_usbd, &my_usbd_lang);
-	err |= usbd_add_descriptor(&my_usbd, &my_usbd_manufacturer);
-	err |= usbd_add_descriptor(&my_usbd, &my_usbd_product);
-	err |= usbd_add_descriptor(&my_usbd, &my_usbd_bos_cap_lpm);
-	err |= usbd_add_descriptor(&my_usbd, &my_usbd_bos_cap_ss);
-	//err |= usbd_add_descriptor(&my_usbd, &my_usbd_serial_number);
-	err |= usbd_add_configuration(&my_usbd, USBD_SPEED_SS, &my_usbd_config);
-	__ASSERT_NO_MSG(err == 0);
+	ret |= usbd_add_descriptor(&usbd, &usbd_lang);
+	ret |= usbd_add_descriptor(&usbd, &usbd_manufacturer);
+	ret |= usbd_add_descriptor(&usbd, &usbd_product);
+	ret |= usbd_add_descriptor(&usbd, &usbd_bos_cap_lpm);
+	ret |= usbd_add_descriptor(&usbd, &usbd_bos_cap_ss);
+	__ASSERT_NO_MSG(ret == 0);
 
-	usbd_device_set_code_triple(&my_usbd, USBD_SPEED_SS, USB_BCC_MISCELLANEOUS, 0x02, 0x01);
+	ret = usbd_add_configuration(&usbd, USBD_SPEED_SS, &usbd_config);
+	__ASSERT_NO_MSG(ret == 0);
 
-	err |= usbd_register_all_classes(&my_usbd, USBD_SPEED_SS, 1);
-	__ASSERT_NO_MSG(err == 0);
+	usbd_device_set_code_triple(&usbd, USBD_SPEED_SS, USB_BCC_MISCELLANEOUS, 0x02, 0x01);
 
-	err |= usbd_init(&my_usbd);
-	__ASSERT_NO_MSG(err == 0);
+	ret = usbd_register_all_classes(&usbd, USBD_SPEED_SS, 1);
+	__ASSERT_NO_MSG(ret == 0);
 
-	err |= usbd_enable(&my_usbd);
-	__ASSERT_NO_MSG(err == 0);
+	ret = usbd_init(&usbd);
+	__ASSERT_NO_MSG(ret == 0);
+
+	ret = usbd_enable(&usbd);
+	__ASSERT_NO_MSG(ret == 0);
+
+	ret = video_get_format(UVC0, VIDEO_EP_IN, &fmt);
+	if (ret != 0) {
+		LOG_ERR("failed to get the video format");
+	}
+
+	/* The buffer address is the FIFO endpoint */
+	vbuf.buffer = (void *)DT_REG_ADDR_BY_NAME(DT_NODELABEL(uvcmanager), fifo);
+	vbuf.size = vbuf.bytesused = fmt.pitch * fmt.height;
+	vbuf.flags = VIDEO_BUF_EOF;
+
+	LOG_DBG("frame %ux%u, %u bytes, addr %p", fmt.width, fmt.height, fmt.pitch * fmt.height, vbuf.buffer);
+
+	/* Only the first buffer needs to be enqueued when using the UVC Manager */
+	ret = video_enqueue(UVC0, VIDEO_EP_IN, &vbuf);
+	if (ret != 0) {
+		LOG_ERR("failed to enqueue the video buffer");
+		return 0;
+	}
 
 	while (true) {
 		usb23_irq_handler(UDC0);
