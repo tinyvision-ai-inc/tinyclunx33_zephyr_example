9603ec8b3c19b38393f9094e16c152a81830d152
Alan Shaju <alanshaju@rideltech.com>
2024-09-18 17:42:19 +0530

    camera exposure support added

--- example_usb_uvc_imx219/src/imx219.c
+++ example_usb_uvc_imx219/src/imx219.c
@@ -3,6 +3,7 @@
 #include <zephyr/drivers/video.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/sys/byteorder.h>
 LOG_MODULE_REGISTER(app2, LOG_LEVEL_DBG);
 
 const struct device *video_dev = DEVICE_DT_GET(DT_NODELABEL(imx219));
@@ -24,7 +25,7 @@ static int gain_control(const struct shell *shell, size_t argc, char **argv) {
 
     scale_value = atoi(argv[1]);
     if (scale_value == 0) {
-        shell_print(shell, "scale_value Usage: camera gain <increase|decrease> <scale>");
+        shell_print(shell, "scale_value Usage: gain <increase|decrease> <scale>");
         return -1;
     }
 
@@ -33,12 +34,12 @@ static int gain_control(const struct shell *shell, size_t argc, char **argv) {
     } else if (strcmp(argv[0], "decrease") == 0) {
         value -= scale_value;
     } else {
-        shell_print(shell, "Invalid argument: %s. Use 'increase' or 'decrease'.", argv[0]);
+        shell_print(shell, "Invalid argument: %s. Use 'increase' or 'decrease'", argv[0]);
         return -1;
     }
 
     if (video_set_ctrl(video_dev, VIDEO_CID_CAMERA_GAIN, &value)) {
-        shell_print(shell, "Unable to set gain value = %d.", value);
+        shell_print(shell, "Unable to set gain value = %d", value);
         return -1;
     }
 
@@ -47,7 +48,41 @@ static int gain_control(const struct shell *shell, size_t argc, char **argv) {
 }
 
 static int exposure_control(const struct shell *shell, size_t argc, char **argv) {
-    shell_print(shell, "Usage: camera exposure <increase|decrease>");
+    int scale_value = 1;
+    uint16_t value;
+
+    if(video_get_ctrl(video_dev,VIDEO_CID_CAMERA_EXPOSURE,&value)){
+        shell_print(shell, "Unable to get current exposure value");
+        return -1;
+    }
+
+    if (strcmp(argv[0], "current_value") == 0) {
+        shell_print(shell, "Current exposure value is %d", value);
+        return 0;
+    }
+
+    scale_value = atoi(argv[1]);
+    if (scale_value == 0) {
+        shell_print(shell, "scale_value Usage: exposure <increase|decrease> <scale>");
+        return -1;
+    }
+
+    if (strcmp(argv[0], "increase") == 0) {
+        value += scale_value;
+        shell_print(shell, "scale_value =  %d\tvalue = %d", scale_value, value);
+    } else if (strcmp(argv[0], "decrease") == 0) {
+        value -= scale_value;
+    } else {
+        shell_print(shell, "Invalid argument: %s. Use 'increase' or 'decrease'", argv[0]);
+        return -1;
+    }
+
+    if (video_set_ctrl(video_dev, VIDEO_CID_CAMERA_EXPOSURE, &value)) {
+        shell_print(shell, "Unable to set exposure value = %d", value);
+        return -1;
+    }
+
+    shell_print(shell, "exposure %s to %d", (strcmp(argv[0], "increase") == 0) ? "increased" : "decreased", sys_be16_to_cpu(value));
     return 0;
 }
 
@@ -59,14 +94,15 @@ SHELL_STATIC_SUBCMD_SET_CREATE(sub_gain,
 );
 
 SHELL_STATIC_SUBCMD_SET_CREATE(sub_exposure,
-    SHELL_CMD_ARG(increase, NULL, "Increase camera exposure", exposure_control, 2, 0),
-    SHELL_CMD_ARG(decrease, NULL, "Decrease camera exposure", exposure_control, 2, 0),
+    SHELL_CMD_ARG(increase, NULL, "Usage: exposur increase", exposure_control, 2, 0),
+    SHELL_CMD_ARG(decrease, NULL, "Usage: exposur decrease", exposure_control, 2, 0),
+    SHELL_CMD_ARG(current_value, NULL, "Usage: exposure current_value", exposure_control, 1, 0),
     SHELL_SUBCMD_SET_END
 );
 
 SHELL_STATIC_SUBCMD_SET_CREATE(cam_ctrls, 
 			SHELL_CMD(gain, &sub_gain, "increase or decrease camera gain", NULL),
-			SHELL_CMD(exposur, &sub_exposure, "increase or decrease camera exposure", NULL),
+			SHELL_CMD(exposure, &sub_exposure, "increase or decrease camera exposure", NULL),
 			SHELL_SUBCMD_SET_END);
 
 SHELL_CMD_REGISTER(imx219, &cam_ctrls, "camera controls", NULL);
