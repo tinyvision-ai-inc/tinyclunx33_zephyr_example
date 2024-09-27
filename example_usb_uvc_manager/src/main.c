31de4e19477fad0784738bc123d73ce6dd4f3cfc
josuah <55351021+josuah@users.noreply.github.com>
2024-09-27 23:48:06 +0200

    Revert "uvc_*: switch to the new API"
    This reverts commit 82ace8b685830ac63215e8aeaa6380e8539d49cc.

--- example_usb_uvc_manager/src/main.c
+++ example_usb_uvc_manager/src/main.c
@@ -11,20 +11,13 @@ int main(void)
 {
 	struct video_buffer vbuf = {0};
 	struct video_format fmt = {0};
-	int err;
+	int ret = 0;
 
-	/* Get the video format once it is selected by the host */
-	while (true) {
-		err = video_get_format(uvc0_dev, VIDEO_EP_IN, &fmt);
-		if (err == 0) {
-			break;
-		}
-		if (err != -EAGAIN) {
-			LOG_ERR("failed to get the video format");
-			return err;
-		}
-
-		k_sleep(K_MSEC(10));
+	/* Get the video format selected by the host */
+	ret = video_get_format(uvc0_dev, VIDEO_EP_IN, &fmt);
+	if (ret != 0) {
+		LOG_ERR("failed to get the video format");
+		return ret;
 	}
 
 	/* The buffer address is the FIFO endpoint */
@@ -35,10 +28,10 @@ int main(void)
 	LOG_DBG("frame %ux%u, %u bytes, addr %p", fmt.width, fmt.height, fmt.pitch * fmt.height, vbuf.buffer);
 
 	/* Only the first buffer needs to be enqueued when using the UVC Manager */
-	err = video_enqueue(uvc0_dev, VIDEO_EP_IN, &vbuf);
-	if (err) {
+	ret = video_enqueue(uvc0_dev, VIDEO_EP_IN, &vbuf);
+	if (ret != 0) {
 		LOG_ERR("failed to enqueue the video buffer");
-		return err;
+		return 0;
 	}
 
 	k_sleep(K_FOREVER);
