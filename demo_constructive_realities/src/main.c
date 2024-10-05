093effa1750aa27e4d28de8a63c867a1900af252
Josuah Demangeon <me@josuah.net>
2024-10-05 12:20:42 +0200

    demo_constructive_realities: not ready yet

--- demo_constructive_realities/src/main.c
+++ demo_constructive_realities/src/main.c
@@ -3,20 +3,21 @@
 #include <zephyr/drivers/video.h>
 #include <zephyr/logging/log.h>
 
-LOG_MODULE_REGISTER(app, LOG_LEVEL_INF);
+LOG_MODULE_REGISTER(app, LOG_LEVEL_DBG);
 
 static const struct device *uvc0_dev = DEVICE_DT_GET(DT_NODELABEL(uvc0));
 static const struct device *uvc1_dev = DEVICE_DT_GET(DT_NODELABEL(uvc1));
-static const struct device *sw_dev = DEVICE_DT_GET(DT_NODELABEL(sw_generator));
+const uintptr_t uvcmanager0_fifo = DT_REG_ADDR_BY_NAME(DT_NODELABEL(uvcmanager0), fifo);
+const uintptr_t uvcmanager1_fifo = DT_REG_ADDR_BY_NAME(DT_NODELABEL(uvcmanager1), fifo);
 
-int uvc0_enqueue_first_frame(struct video_buffer *vbuf)
+int uvcmanager_enqueue_first_frame(const struct device *uvc_dev, uintptr_t fifo, struct video_buffer *vbuf)
 {
 	struct video_format fmt = {0};
 	int ret;
 
-	ret = video_get_format(uvc0_dev, VIDEO_EP_IN, &fmt);
+	ret = video_get_format(uvc_dev, VIDEO_EP_IN, &fmt);
 	if (ret == -EAGAIN) {
-		return false;
+		return ret;
 	}
 	if (ret) {
 		LOG_ERR("failed to get the video format");
@@ -24,130 +25,42 @@ int uvc0_enqueue_first_frame(struct video_buffer *vbuf)
 	}
 
 	/* The buffer address is the FIFO endpoint */
-	vbuf->buffer = (void *)DT_REG_ADDR_BY_NAME(DT_NODELABEL(uvcmanager0), fifo);
+	vbuf->buffer = (void *)fifo;
 	vbuf->size = vbuf->bytesused = fmt.pitch * fmt.height;
 	vbuf->flags = VIDEO_BUF_EOF;
 
 	LOG_INF("frame %ux%u, %u bytes, addr %p", fmt.width, fmt.height, fmt.pitch * fmt.height, vbuf->buffer);
 
 	/* Only the first buffer needs to be enqueued when using the UVC Manager */
-	ret = video_enqueue(uvc0_dev, VIDEO_EP_IN, vbuf);
+	ret = video_enqueue(uvc_dev, VIDEO_EP_IN, vbuf);
 	if (ret) {
-		LOG_ERR("UVC0: failed to enqueue the first frame");
+		LOG_ERR("%s: failed to enqueue the first frame", uvc_dev->name);
 		return ret;
 	}
-	LOG_DBG("enqueued %p to %s", vbuf, uvc0_dev->name);
+	LOG_DBG("enqueued %p to %s", vbuf->buffer, uvc_dev->name);
 
-	LOG_INF("UVC0 ready for I/O");
-
+	LOG_INF("FIFO 0x%08lx ready for I/O", fifo);
 	return true;
 }
-
-int uvc1_enqueue_first_frame(void)
-{
-	struct video_format fmt = {0};
-	struct video_buffer *vbuf;
-	int ret;
-
-	ret = video_get_format(uvc1_dev, VIDEO_EP_IN, &fmt);
-	if (ret == -EAGAIN) {
-		return false;
-	}
-	if (ret) {
-		LOG_ERR("failed to get the video format");
-		return ret;
-	}
-
-	LOG_INF("Allocating a video buffer of %u bytes for UVC1", fmt.pitch * fmt.height);
-
-	/* If this gets stuck, you might need to increase CONFIG_VIDEO_BUFFER_POOL_SZ_MAX */
-	vbuf = video_buffer_alloc(fmt.pitch * fmt.height);
-	if (vbuf == NULL) {
-		LOG_ERR("could not allocate the video buffer");
-		return -ENOMEM;
-	}
-
-	for (size_t i = 0; i < vbuf->size; i++) {
-		vbuf->buffer[i] ^= 0xff;
-	}
-	vbuf->bytesused = vbuf->size;
-	vbuf->flags = VIDEO_BUF_EOF;
-
-	ret = video_enqueue(sw_dev, VIDEO_EP_OUT, vbuf);
-	if (ret != 0) {
-		LOG_ERR("could not enqueue video buffer to %s", sw_dev->name);
-		return ret;
-	}
-	LOG_DBG("enqueued %p to %s", vbuf, sw_dev->name);
-
-	LOG_INF("UVC1 ready for I/O");
-
-	return true;
-}
-
-void uvc1_io_thread(void *, void *, void *)
-{
-	struct video_buffer *vbuf;
-	int ret;
-
-	/* UVC needs the poll API implemented */
-	while (true) {
-		ret = video_dequeue(sw_dev, VIDEO_EP_OUT, &vbuf, K_FOREVER);
-		if (ret != 0) {
-			LOG_ERR("could not dequeue video buffer from %s", sw_dev->name);
-			return;
-		}
-		LOG_DBG("dequeued %p from %s", vbuf, sw_dev->name);
-
-		ret = video_enqueue(uvc1_dev, VIDEO_EP_IN, vbuf);
-		if (ret != 0) {
-			LOG_ERR("could not enqueue video buffer to %s", uvc1_dev->name);
-			return;
-		}
-		LOG_DBG("enqueued %p to %s", vbuf, uvc1_dev->name);
-
-		ret = video_dequeue(uvc1_dev, VIDEO_EP_IN, &vbuf, K_FOREVER);
-		if (ret != 0) {
-			LOG_ERR("could not dequeue video buffer from %s", uvc1_dev->name);
-			return;
-		}
-		LOG_DBG("dequeued %p from %s", vbuf, uvc1_dev->name);
-
-		ret = video_enqueue(sw_dev, VIDEO_EP_OUT, vbuf);
-		if (ret != 0) {
-			LOG_ERR("could not enqueue video buffer to %s", sw_dev->name);
-			return;
-		}
-		LOG_DBG("enqueued %p to %s", vbuf, sw_dev->name);
-	}
-}
-
-K_THREAD_DEFINE(uvc1_io, 1024, uvc1_io_thread, NULL, NULL, NULL, CONFIG_MAIN_THREAD_PRIORITY, 0, 0);
 
 int main(void)
 {
-	struct video_buffer vbuf = {0};
+	struct video_buffer vbuf0 = {0}, vbuf1 = {0};
 	int ret;
 
 	/* Get the video format once it is selected by the host */
-	for (bool uvc0_ready = 0, uvc1_ready = 0; !uvc0_ready || !uvc1_ready;) {
-		if (!uvc0_ready) {
-			ret = uvc0_enqueue_first_frame(&vbuf);
-			if (ret < 0) {
-				LOG_ERR("UVC0: failed to enqueue the first frame");
-				return ret;
-			}
-			uvc0_ready = ret;
+	for (bool uvc0_ready = false, uvc1_ready = false; !uvc0_ready || !uvc1_ready;) {
+		ret = uvcmanager_enqueue_first_frame(uvc0_dev, uvcmanager0_fifo, &vbuf0);
+		if (ret < 0) {
+			return ret;
 		}
+		uvc1_ready = ret;
 
-		if (!uvc1_ready) {
-			ret = uvc1_enqueue_first_frame();
-			if (ret < 0) {
-				LOG_ERR("UVC1: failed to enqueue the first frame");
-				return ret;
-			}
-			uvc1_ready = ret;
+		ret = uvcmanager_enqueue_first_frame(uvc1_dev, uvcmanager1_fifo, &vbuf1);
+		if (ret < 0) {
+			return ret;
 		}
+		uvc1_ready = ret;
 
 		k_sleep(K_MSEC(10));
 	}
