56ec96eee2180e042d041888ef47437fc4306233
Josuah Demangeon <me@josuah.net>
2024-11-19 01:58:59 +0100

    get the firmware to build and boot

--- example_usb_uvc_imx219/src/main.c
+++ example_usb_uvc_imx219/src/main.c
@@ -5,23 +5,23 @@
 
 LOG_MODULE_REGISTER(app, LOG_LEVEL_DBG);
 
-static const struct device *uvc0_dev = DEVICE_DT_GET(DT_NODELABEL(uvc0));
+static const struct device *uvc_dev = DEVICE_DT_GET(DT_NODELABEL(uvc0));
 
 int main(void)
 {
 	struct video_buffer vbuf = {0};
 	struct video_format fmt = {0};
-	int err;
+	int ret;
 
 	/* Get the video format once it is selected by the host */
 	while (true) {
-		err = video_get_format(uvc0_dev, VIDEO_EP_IN, &fmt);
-		if (err == 0) {
+		ret = video_get_format(uvc_dev, VIDEO_EP_IN, &fmt);
+		if (ret == 0) {
 			break;
 		}
-		if (err != -EAGAIN) {
-			LOG_ERR("failed to get the video format");
-			return err;
+		if (ret != -EAGAIN) {
+			LOG_ERR("Failed to get the video format");
+			return ret;
 		}
 
 		k_sleep(K_MSEC(10));
@@ -30,15 +30,14 @@ int main(void)
 	/* The buffer address is the FIFO endpoint */
 	vbuf.buffer = (void *)DT_REG_ADDR_BY_NAME(DT_NODELABEL(uvcmanager0), fifo);
 	vbuf.size = vbuf.bytesused = fmt.pitch * fmt.height;
-	vbuf.flags = VIDEO_BUF_EOF;
 
 	LOG_DBG("frame %ux%u, %u bytes, addr %p", fmt.width, fmt.height, fmt.pitch * fmt.height, vbuf.buffer);
 
 	/* Only the first buffer needs to be enqueued when using the UVC Manager */
-	err = video_enqueue(uvc0_dev, VIDEO_EP_IN, &vbuf);
-	if (err) {
+	ret = video_enqueue(uvc_dev, VIDEO_EP_IN, &vbuf);
+	if (ret) {
 		LOG_ERR("failed to enqueue the video buffer");
-		return err;
+		return ret;
 	}
 
 	k_sleep(K_FOREVER);
--- example_usb_uvc_imx219/src/usb.c
+++ example_usb_uvc_imx219/src/usb.c
@@ -12,31 +12,11 @@ USBD_DEVICE_DEFINE(usbd, udc0_dev, 0x1209, 0x0001);
 USBD_DESC_MANUFACTURER_DEFINE(usbd_manufacturer, "tinyVision.ai");
 USBD_DESC_PRODUCT_DEFINE(usbd_product, "tinyCLUNX33");
 USBD_DESC_LANG_DEFINE(usbd_lang);
+USBD_DESC_CONFIG_DEFINE(usbd_fs_cfg_desc, "FullSpeed Configuration");
+USBD_DESC_CONFIG_DEFINE(usbd_hs_cfg_desc, "HighSpeed Configuration");
 
-static const struct usb_bos_capability_superspeed_usb bos_cap_ss = {
-	.bLength = sizeof(struct usb_bos_capability_superspeed_usb),
-	.bDescriptorType = USB_DESC_DEVICE_CAPABILITY,
-	.bDevCapabilityType = USB_BOS_CAPABILITY_SUPERSPEED_USB,
-	.bmAttributes = 0,
-	.wSpeedsSupported = sys_cpu_to_le16(USB_BOS_SPEED_SUPERSPEED_GEN1 |
-					    USB_BOS_SPEED_HIGHSPEED | USB_BOS_SPEED_FULLSPEED),
-	.bFunctionnalSupport = 1,
-	.bU1DevExitLat = 10,
-	.wU2DevExitLat = sys_cpu_to_le16(1023),
-};
-USBD_DESC_BOS_DEFINE(usbd_bos_cap_ss, sizeof(bos_cap_ss), &bos_cap_ss);
-
-static const struct usb_bos_capability_lpm bos_cap_lpm = {
-	.bLength = sizeof(struct usb_bos_capability_lpm),
-	.bDescriptorType = USB_DESC_DEVICE_CAPABILITY,
-	.bDevCapabilityType = USB_BOS_CAPABILITY_EXTENSION,
-	.bmAttributes = USB_BOS_ATTRIBUTES_LPM,
-};
-USBD_DESC_BOS_DEFINE(usbd_bos_cap_lpm, sizeof(bos_cap_lpm), &bos_cap_lpm);
-
-USBD_CONFIGURATION_DEFINE(usbd_config_fs, USB_SCD_SELF_POWERED, 100);
-USBD_CONFIGURATION_DEFINE(usbd_config_hs, USB_SCD_SELF_POWERED, 100);
-USBD_CONFIGURATION_DEFINE(usbd_config_ss, USB_SCD_SELF_POWERED, 100);
+USBD_CONFIGURATION_DEFINE(usbd_config_fs, USB_SCD_SELF_POWERED, 100, &usbd_fs_cfg_desc);
+USBD_CONFIGURATION_DEFINE(usbd_config_hs, USB_SCD_SELF_POWERED, 100, &usbd_hs_cfg_desc);
 
 int app_usb_init(void)
 {
@@ -58,16 +38,6 @@ int app_usb_init(void)
 		LOG_ERR("failed to add product descriptor");
 		return err;
 	}
-	err = usbd_add_descriptor(&usbd, &usbd_bos_cap_lpm);
-	if (err) {
-		LOG_ERR("failed to add bos_cap_lpm descriptor");
-		return err;
-	}
-	err = usbd_add_descriptor(&usbd, &usbd_bos_cap_ss);
-	if (err) {
-		LOG_ERR("failed to add bos_cap_ss descriptor");
-		return err;
-	}
 
 	LOG_DBG("Adding USB configuration");
 	err = usbd_add_configuration(&usbd, USBD_SPEED_FS, &usbd_config_fs);
@@ -80,15 +50,9 @@ int app_usb_init(void)
 		LOG_ERR("failed to add HighSpeed, configuration");
 		return err;
 	}
-	err = usbd_add_configuration(&usbd, USBD_SPEED_SS, &usbd_config_ss);
-	if (err) {
-		LOG_ERR("failed to add SuperSpeed, configuration");
-		return err;
-	}
 
 	usbd_device_set_code_triple(&usbd, USBD_SPEED_FS, USB_BCC_MISCELLANEOUS, 0x02, 0x01);
 	usbd_device_set_code_triple(&usbd, USBD_SPEED_HS, USB_BCC_MISCELLANEOUS, 0x02, 0x01);
-	usbd_device_set_code_triple(&usbd, USBD_SPEED_SS, USB_BCC_MISCELLANEOUS, 0x02, 0x01);
 
 	LOG_DBG("Adding USB classes");
 	err = usbd_register_all_classes(&usbd, USBD_SPEED_FS, 1);
@@ -99,11 +63,6 @@ int app_usb_init(void)
 	err = usbd_register_all_classes(&usbd, USBD_SPEED_HS, 1);
 	if (err) {
 		LOG_ERR("failed to register HighSpeed USB clases");
-		return err;
-	}
-	err = usbd_register_all_classes(&usbd, USBD_SPEED_SS, 1);
-	if (err) {
-		LOG_ERR("failed to register SuperSpeed USB clases");
 		return err;
 	}
 
