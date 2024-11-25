b477c6ac534bb431ae86067b03fcd58b7c382ef1
Josuah Demangeon <me@josuah.net>
2024-11-25 02:51:16 +0100

    uvc_imx219: add missing BOS and LPM descriptors

--- example_usb_uvc_imx219/src/usb.c
+++ example_usb_uvc_imx219/src/usb.c
@@ -9,12 +9,33 @@ static const struct device *const udc0_dev = DEVICE_DT_GET(DT_NODELABEL(zephyr_u
 
 USBD_DEVICE_DEFINE(usbd, udc0_dev, 0x1209, 0x0001);
 
+static const struct usb_bos_capability_superspeed_usb bos_cap_ss = {
+	.bLength = sizeof(struct usb_bos_capability_superspeed_usb),
+	.bDescriptorType = USB_DESC_DEVICE_CAPABILITY,
+	.bDevCapabilityType = USB_BOS_CAPABILITY_SUPERSPEED_USB,
+	.bmAttributes = 0,
+	.wSpeedsSupported = sys_cpu_to_le16(USB_BOS_SPEED_SUPERSPEED_GEN1 |
+					    USB_BOS_SPEED_HIGHSPEED | USB_BOS_SPEED_FULLSPEED),
+	.bFunctionnalSupport = 1,
+	.bU1DevExitLat = 10,
+	.wU2DevExitLat = sys_cpu_to_le16(1023),
+};
+
+static const struct usb_bos_capability_lpm bos_cap_lpm = {
+	.bLength = sizeof(struct usb_bos_capability_lpm),
+	.bDescriptorType = USB_DESC_DEVICE_CAPABILITY,
+	.bDevCapabilityType = USB_BOS_CAPABILITY_EXTENSION,
+	.bmAttributes = USB_BOS_ATTRIBUTES_LPM,
+};
+
 USBD_DESC_MANUFACTURER_DEFINE(usbd_manufacturer, "tinyVision.ai");
 USBD_DESC_PRODUCT_DEFINE(usbd_product, "tinyCLUNX33");
 USBD_DESC_LANG_DEFINE(usbd_lang);
 USBD_DESC_CONFIG_DEFINE(usbd_fs_cfg_desc, "FullSpeed Configuration");
 USBD_DESC_CONFIG_DEFINE(usbd_hs_cfg_desc, "HighSpeed Configuration");
 USBD_DESC_CONFIG_DEFINE(usbd_ss_cfg_desc, "SuperSpeed Configuration");
+USBD_DESC_BOS_DEFINE(usbd_bos_cap_ss, sizeof(bos_cap_ss), &bos_cap_ss);
+USBD_DESC_BOS_DEFINE(usbd_bos_cap_lpm, sizeof(bos_cap_lpm), &bos_cap_lpm);
 
 USBD_CONFIGURATION_DEFINE(usbd_config_fs, USB_SCD_SELF_POWERED, 100, &usbd_fs_cfg_desc);
 USBD_CONFIGURATION_DEFINE(usbd_config_hs, USB_SCD_SELF_POWERED, 100, &usbd_hs_cfg_desc);
@@ -38,6 +59,16 @@ int app_usb_init(void)
 	ret = usbd_add_descriptor(&usbd, &usbd_product);
 	if (ret < 0) {
 		LOG_ERR("failed to add product descriptor");
+		return ret;
+	}
+	ret = usbd_add_descriptor(&usbd, &usbd_bos_cap_lpm);
+	if (ret < 0) {
+		LOG_ERR("failed to add bos_cap_lpm descriptor");
+		return ret;
+	}
+	ret = usbd_add_descriptor(&usbd, &usbd_bos_cap_ss);
+	if (ret < 0) {
+		LOG_ERR("failed to add bos_cap_ss descriptor");
 		return ret;
 	}
 
