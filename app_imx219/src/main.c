20096d183cc7a4db8cb3ea87dd858fef06e0a8d2
Josuah Demangeon <me@josuah.net>
2025-03-07 01:01:57 +0100

    app_imx219: light polishing

--- app_imx219/src/main.c
+++ app_imx219/src/main.c
@@ -6,7 +6,7 @@
 LOG_MODULE_REGISTER(app_main, LOG_LEVEL_INF);
 
 static const struct device *const uvc0_dev = DEVICE_DT_GET(DT_NODELABEL(uvc0));
-static const struct device *const source0_dev = DEVICE_DT_GET(DT_NODELABEL(uvcmanager0));
+static const struct device *const uvcmanager0_dev = DEVICE_DT_GET(DT_NODELABEL(uvcmanager0));
 
 int app_usb_init(void);
 
@@ -14,7 +14,9 @@ int main(void)
 {
 	int ret;
 
-	uvc_set_video_dev(uvc0_dev, source0_dev);
+	uvc_set_video_dev(uvc0_dev, uvcmanager0_dev);
+
+	LOG_INF("Initializing USB and associated USB classes");
 
 	ret = app_usb_init();
 	if (ret != 0) {
@@ -22,11 +24,15 @@ int main(void)
 		return ret;
 	}
 
-	ret = video_set_ctrl(source0_dev, VIDEO_CID_GAIN, (void *)555);
+	LOG_INF("Started USB, configuring video stream default values");
+
+	ret = video_set_ctrl(uvcmanager0_dev, VIDEO_CID_GAIN, (void *)555);
 	if (ret != 0) {
-		LOG_ERR("Cannot set the default gain for %s", source0_dev->name);
+		LOG_ERR("Cannot set the default gain for %s", uvcmanager0_dev->name);
 		return ret;
 	}
+
+	LOG_INF("Done, exiting main() and letting the USB stack run");
 
 	return 0;
 }
